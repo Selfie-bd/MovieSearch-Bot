0b2a79fafa75cb784072d30016df0c68262d12b7
Selfie SD <webtamil0@gmail.com>
2022-03-03 06:26:12 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -90,8 +90,8 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('🎥 Search again ', switch_inline_query_current_chat=query),
+            InlineKeyboardButton('More Bots❗', url='https://t.me/groupdcbots/122')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
